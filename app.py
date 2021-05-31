f2b5b2c30595c94b5fe68e82b903e1485ed11fa7
haakensonb <unntamedhawk44@gmail.com>
2021-05-31 15:03:30 -0700

    Able to download and hash img from subreddit
    Update gitignore
    
    Add config file
    
    Add model for image records
    
    Add database util functions

--- app.py
+++ app.py
@@ -1,5 +1,27 @@
 import praw
+import requests
+import random
+import imagehash
+from PIL import Image
+import os
 from secrets import REDDIT_ID, REDDIT_SECRET, REDDIT_USER_AGENT
+
+DIR = "images"
+if not os.path.exists(DIR):
+    os.makedirs(DIR)
 
 if __name__ == "__main__":
     print("Hello World")
+    reddit = praw.Reddit(
+        client_id = REDDIT_ID,
+        client_secret = REDDIT_SECRET,
+        user_agent = REDDIT_USER_AGENT
+    )
+    posts = [post for post in reddit.subreddit("LiminalSpace").hot(limit=25)]
+    filtered_posts = [post for post in posts if post.url.startswith("https://i.")]
+    post = random.choice(filtered_posts)
+    file_loc = f"./{DIR}/{post.title}.jpg"
+    with open(file_loc, "wb") as f:
+        f.write(requests.get(post.url).content)
+    img_hash = imagehash.phash(Image.open(file_loc))
+    print(img_hash)
